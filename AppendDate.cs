1be3e8ea95cf6c436459a45f45b9a3859c0d0ca1
letulip <ivladimirskiy@ya.ru>
2016-12-02 10:13:29 +0300

    Class AppendDate added, Program actualized for desctop file saving

---
+++ AppendDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCapture
+{
+    static public class AppendDate
+    {
+        public static string AppendTimeStamp(this string fileName)
+        {
+            string filePath = fileName.Replace(".", DateTime.Now.ToString(" yyyy-MM-dd HH.mm.ss."));
+
+            return filePath;
+        }
+    }
+}
