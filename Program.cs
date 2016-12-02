14aeb7a286dac0d33ac1255dafd67e05823da94f
letulip <ivladimirskiy@ya.ru>
2016-12-02 10:02:12 +0300

    Файл сохраняется на рабочий стол со штампом даты. Добавлен класс для фиксирования штампа даты в названии файла. Актуализирован файл проекта для сохранения файла на рабочий стол.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using IWshRuntimeLibrary;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Windows.Forms;
@@ -8,66 +10,90 @@ namespace ScreenCapture
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            _hookID = SetHook(_proc);
-            Application.Run();
-            
-            UnhookWindowsHookEx(_hookID);
-        }
+            // Specify the directory you want to manipulate.
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\hello.txt";
 
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private const int VK_F1 = 0x70;
-        private static LowLevelKeyboardProc _proc = HookCallback;
-        private static IntPtr _hookID = IntPtr.Zero;
-
-        private static IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
+            try
             {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-            }
-        }
-
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0)
-            {
-                Keys number = (Keys)Marshal.ReadInt32(lParam);
-                //MessageBox.Show(number.ToString());
-                if (number == Keys.PrintScreen)
+                using (FileStream fs = System.IO.File.Create(filePath.AppendTimeStamp()))
                 {
-                    if (wParam == (IntPtr)261 && Keys.Alt == Control.ModifierKeys && number == Keys.PrintScreen)
-                    {
-                        // Alt+PrintScreen
-                    }
-                    else if (wParam == (IntPtr)257 && number == Keys.PrintScreen)
-                    {
-                        // PrintScreen
-                    }
+                    Byte[] info = new UTF8Encoding(true).GetBytes("Hello there! I'm your first saved file!");
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
                 }
             }
 
-            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    _hookID = SetHook(_proc);
+        //    Application.Run();
+
+        //    UnhookWindowsHookEx(_hookID);
+        //}
+
+        //private const int WH_KEYBOARD_LL = 13;
+        //private const int WM_KEYDOWN = 0x0100;
+        //private const int VK_F1 = 0x70;
+        //private static LowLevelKeyboardProc _proc = HookCallback;
+        //private static IntPtr _hookID = IntPtr.Zero;
+
+        //private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        //{
+        //    using (Process curProcess = Process.GetCurrentProcess())
+        //    using (ProcessModule curModule = curProcess.MainModule)
+        //    {
+        //        return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        //    }
+        //}
+
+        //private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        //private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        //{
+        //    if (nCode >= 0)
+        //    {
+        //        Keys number = (Keys)Marshal.ReadInt32(lParam);
+        //        //MessageBox.Show(number.ToString());
+        //        if (number == Keys.PrintScreen)
+        //        {
+        //            if (wParam == (IntPtr)261 && Keys.Alt == Control.ModifierKeys && number == Keys.PrintScreen)
+        //            {
+        //                // Alt+PrintScreen
+        //            }
+        //            else if (wParam == (IntPtr)257 && number == Keys.PrintScreen)
+        //            {
+        //                // PrintScreen
+        //            }
+        //        }
+        //    }
+
+        //    return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        //}
+
+        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        //[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
