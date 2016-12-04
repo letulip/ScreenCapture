83e54fb430234ff0cd9fd9f77880a96d38354408
letulip <ivladimirskiy@ya.ru>
2016-12-04 13:55:33 +0300

    Class ApendDate and Program modified for multiscreen ability

--- AppendDate.cs
+++ AppendDate.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ScreenCapture
 {
     static public class AppendDate
     {
-        public static string AppendTimeStamp(this string fileName)
+        public static string AppendTimeStamp(this string fileName, int n)
         {
-            string filePath = fileName.Replace(".", DateTime.Now.ToString(" yyyy-MM-dd HH.mm.ss."));
+            string filePath = fileName.Replace("Y", Convert.ToString(n));
+            filePath = filePath.Replace(".", DateTime.Now.ToString(" yyyy-MM-dd HH.mm.ss."));
 
             return filePath;
         }
--- Program.cs
+++ Program.cs
@@ -12,27 +12,23 @@ namespace ScreenCapture
 {
     class Program
     {
-        
+
         public static void Main()
         {
             // Specify the directory you want to manipulate.
-            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Printscreen.png";
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Print of screen №Y.png";
 
             try
             {
-                using (FileStream fs = System.IO.File.Create(filePath))
+                for (int i = 0; i < Screen.AllScreens.Length; i++)
                 {
-                    Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                    Bitmap printscreen = new Bitmap(Screen.AllScreens[i].Bounds.Width, Screen.AllScreens[i].Bounds.Height);
 
                     Graphics graphics = Graphics.FromImage(printscreen as Image);
 
-                    graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+                    graphics.CopyFromScreen(Screen.AllScreens[i].Bounds.X, Screen.AllScreens[i].Bounds.Y, 0, 0, printscreen.Size);
 
-                    printscreen.Save(filePath.AppendTimeStamp(), ImageFormat.Png);
-
-                    //Byte[] info = new UTF8Encoding(true).GetBytes("Hello there! I'm your first saved file!");
-                    //// Add some information to the file.
-                    //fs.Write(info, 0, info.Length);
+                    printscreen.Save(filePath.AppendTimeStamp(i+1), ImageFormat.Png);
                 }
             }
 
@@ -41,70 +37,5 @@ namespace ScreenCapture
                 Console.WriteLine(ex.ToString());
             }
         }
-
-
-
-
-        //static void Main()
-        //{
-        //    Application.EnableVisualStyles();
-        //    Application.SetCompatibleTextRenderingDefault(false);
-        //    _hookID = SetHook(_proc);
-        //    Application.Run();
-
-        //    UnhookWindowsHookEx(_hookID);
-        //}
-
-        //private const int WH_KEYBOARD_LL = 13;
-        //private const int WM_KEYDOWN = 0x0100;
-        //private const int VK_F1 = 0x70;
-        //private static LowLevelKeyboardProc _proc = HookCallback;
-        //private static IntPtr _hookID = IntPtr.Zero;
-
-        //private static IntPtr SetHook(LowLevelKeyboardProc proc)
-        //{
-        //    using (Process curProcess = Process.GetCurrentProcess())
-        //    using (ProcessModule curModule = curProcess.MainModule)
-        //    {
-        //        return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-        //    }
-        //}
-
-        //private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        //private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        //{
-        //    if (nCode >= 0)
-        //    {
-        //        Keys number = (Keys)Marshal.ReadInt32(lParam);
-        //        //MessageBox.Show(number.ToString());
-        //        if (number == Keys.PrintScreen)
-        //        {
-        //            if (wParam == (IntPtr)261 && Keys.Alt == Control.ModifierKeys && number == Keys.PrintScreen)
-        //            {
-        //                // Alt+PrintScreen
-        //            }
-        //            else if (wParam == (IntPtr)257 && number == Keys.PrintScreen)
-        //            {
-        //                // PrintScreen
-        //            }
-        //        }
-        //    }
-
-        //    return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
-        //}
-
-        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
-        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //[return: MarshalAs(UnmanagedType.Bool)]
-        //private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        //[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        //[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        //private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
