6b8e2ad7f25417fbd845f9d5e457fa305be9ff7a
letulip <ivladimirskiy@ya.ru>
2016-12-04 20:30:36 +0300

    Tray notifier added.

--- Form1.cs
+++ Form1.cs
@@ -11,12 +11,19 @@ namespace ScreenCapture
             // делаем видимой нашу иконку в трее
             notifyIcon1.Visible = true;
             ShowInTaskbar = false;
+            notifyIcon1.BalloonTipTitle = "Программа была спрятана";
+            notifyIcon1.BalloonTipText = "Обратите внимание что программа была спрятана в трей и продолжит свою работу.";
+            notifyIcon1.ShowBalloonTip(5000); // Параметром указываем количество миллисекунд, которое будет показываться подсказка
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
             if (WindowState == FormWindowState.Minimized)
                 Hide();
+
+            notifyIcon1.BalloonTipTitle = "Программа была спрятана";
+            notifyIcon1.BalloonTipText = "Обратите внимание что программа была спрятана в трей и продолжит свою работу.";
+            notifyIcon1.ShowBalloonTip(5000); // Параметром указываем количество миллисекунд, которое будет показываться подсказка
         }
 
 
@@ -36,7 +43,7 @@ namespace ScreenCapture
             // добавляем Эвент или событие по 1му клику мышки, 
             //вызывая функцию  notifyIcon1_MouseClick
             this.notifyIcon1.MouseClick += new MouseEventHandler(notifyIcon1_MouseClick);
-            this.ShowInTaskbar = true;
+            this.ShowInTaskbar = false;
             WindowState = FormWindowState.Normal;
         }
 
