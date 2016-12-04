f9c763875d98ad8cfa73a2aa2700c964c332d71b
letulip <ivladimirskiy@ya.ru>
2016-12-04 17:45:07 +0300

    Main fixed, SC method incapsulated

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ScreenCapture
@@ -23,11 +16,12 @@ namespace ScreenCapture
 
         private void btnScrshot_Click(object sender, EventArgs e)
         {
-            
+            SCMethod.MakeSC();   
         }
 
         private void btnCloseProgram_Click(object sender, EventArgs e)
         {
+            
             this.Close();
         }
 
--- Program.cs
+++ Program.cs
@@ -15,28 +15,30 @@ namespace ScreenCapture
         
         public static void Main()
         {
+            Form SCForm = new Form1();
+            
             
             // Specify the directory you want to manipulate.
-            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Screen shot_screen №Y.png";
+            //string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Screen shot_screen №Y.png";
 
-            try
-            {
-                for (int i = 0; i < Screen.AllScreens.Length; i++)
-                {
-                    Bitmap printscreen = new Bitmap(Screen.AllScreens[i].Bounds.Width, Screen.AllScreens[i].Bounds.Height);
+            //try
+            //{
+            //    for (int i = 0; i < Screen.AllScreens.Length; i++)
+            //    {
+            //        Bitmap printscreen = new Bitmap(Screen.AllScreens[i].Bounds.Width, Screen.AllScreens[i].Bounds.Height);
 
-                    Graphics graphics = Graphics.FromImage(printscreen as Image);
+            //        Graphics graphics = Graphics.FromImage(printscreen as Image);
 
-                    graphics.CopyFromScreen(Screen.AllScreens[i].Bounds.X, Screen.AllScreens[i].Bounds.Y, 0, 0, printscreen.Size);
+            //        graphics.CopyFromScreen(Screen.AllScreens[i].Bounds.X, Screen.AllScreens[i].Bounds.Y, 0, 0, printscreen.Size);
 
-                    printscreen.Save(filePath.AppendTimeStamp(i+1), ImageFormat.Png);
-                }
-            }
+            //        printscreen.Save(filePath.AppendTimeStamp(i+1), ImageFormat.Png);
+            //    }
+            //}
 
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
+            //}
         }
     }
 }
